e574ad69fd114c53160e0a9a2b7591ce4b01e5d9
KevinVanWezel <21973@ma-web.nl>
2018-01-15 16:07:24 +0100

    m

---
+++ String.cs
@@ -0,0 +1,15 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strings
+{
+
+    public class Movement
+    {
+        public const string VERTICAL = "Vertical";
+        public const string HORIZONTAL = "Horizontal";
+        
+    }
+}
+*/
